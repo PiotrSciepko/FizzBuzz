03537097cdcb6318f2a8023cdecfc532155eb290
piotrs <piotrek.priv@gmail.com>
2020-07-10 20:05:03 +0200

    1st solution

--- src/FizzBuzz/FizzBuzz.java
+++ src/FizzBuzz/FizzBuzz.java
@@ -3,6 +3,19 @@ package FizzBuzz;
 public class FizzBuzz {
 
     public static void main(String[] args) {
-	// write your code here
+
+        for (int i = 1; i <= 20; i++) {
+            if (i % 3 == 0) {
+                if (i % 5 == 0) {
+                    System.out.println("FizzBuzz");
+                } else {
+                    System.out.println("Fizz");
+                }
+            } else if (i%5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
 }
