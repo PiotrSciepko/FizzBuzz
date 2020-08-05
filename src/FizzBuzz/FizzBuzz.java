295c73e26de9cbd441d793d9d5f9b05e28c0f8aa
piotrs <piotrek.priv@gmail.com>
2020-08-05 18:02:13 +0200

    3rd solution, using ternary operator

--- src/FizzBuzz/FizzBuzz.java
+++ src/FizzBuzz/FizzBuzz.java
@@ -4,17 +4,11 @@ public class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 100; i++) {
-            if (i % 15 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
-            }
+        for(int i = 1; i <= 100; i++) {
+            String str = i % 15 == 0 ? "FizzBuzz" : (i % 3 == 0 ? "Fizz" : (i % 5 == 0 ? "Buzz" : String.valueOf(i)));
+            System.out.println(str);
         }
+
     }
 }
 
