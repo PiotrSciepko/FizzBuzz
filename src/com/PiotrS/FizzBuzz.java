fb2e15870d87ef5fdf6000f2a8c56f6c954db990
piotrs <piotrek.priv@gmail.com>
2020-08-05 18:52:06 +0200

    package config modified

--- src/com/PiotrS/FizzBuzz.java
+++ src/com/PiotrS/FizzBuzz.java
@@ -1,11 +1,11 @@
-package FizzBuzz;
+package com.PiotrS;
 
 public class FizzBuzz {
 
     public static void main(String[] args) {
 
         for(int i = 1; i <= 100; i++) {
-            String str = i % 15 == 0 ? "FizzBuzz" : (i % 3 == 0 ? "Fizz" : (i % 5 == 0 ? "Buzz" : String.valueOf(i)));
+            String str = i % 15 == 0 ? "com/PiotrS" : (i % 3 == 0 ? "Fizz" : (i % 5 == 0 ? "Buzz" : String.valueOf(i)));
             System.out.println(str);
         }
 
