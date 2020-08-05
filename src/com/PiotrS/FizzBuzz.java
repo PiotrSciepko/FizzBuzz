3e6bed748a6c39b5341831c867db00d45c11c3c5
piotrs <piotrek.priv@gmail.com>
2020-08-05 18:57:31 +0200

    3rd solution, using ternary operator continued

--- src/com/PiotrS/FizzBuzz.java
+++ src/com/PiotrS/FizzBuzz.java
@@ -5,7 +5,7 @@ public class FizzBuzz {
     public static void main(String[] args) {
 
         for(int i = 1; i <= 100; i++) {
-            String str = i % 15 == 0 ? "com/PiotrS" : (i % 3 == 0 ? "Fizz" : (i % 5 == 0 ? "Buzz" : String.valueOf(i)));
+            String str = i % 15 == 0 ? "FizzBuzz" : (i % 3 == 0 ? "Fizz" : (i % 5 == 0 ? "Buzz" : String.valueOf(i)));
             System.out.println(str);
         }
 
