dc0bf3d32fa62dcbf66b9cad63f9c8163e9e3c0c
piotrs <piotrek.priv@gmail.com>
2020-08-07 19:57:13 +0200

    4th solution, string concatenation method

--- src/com/PiotrS/FizzBuzz.java
+++ src/com/PiotrS/FizzBuzz.java
@@ -4,8 +4,18 @@ public class FizzBuzz {
 
     public static void main(String[] args) {
 
+        String str;
         for(int i = 1; i <= 100; i++) {
-            String str = i % 15 == 0 ? "FizzBuzz" : (i % 3 == 0 ? "Fizz" : (i % 5 == 0 ? "Buzz" : String.valueOf(i)));
+            str = "";
+            if (i % 3 == 0) {
+                str = "Fizz";
+            }
+            if (i % 5 == 0) {
+                str += "Buzz";
+            }
+            if (str.isEmpty()) {
+                str += i;
+            }
             System.out.println(str);
         }
 
