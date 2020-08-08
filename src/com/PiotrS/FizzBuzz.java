05073033ed5f9793d6fc6d68609441b325a91674
piotrs <piotrek.priv@gmail.com>
2020-08-08 19:33:54 +0200

    4th solution, string concatenation method cont

--- src/com/PiotrS/FizzBuzz.java
+++ src/com/PiotrS/FizzBuzz.java
@@ -7,15 +7,9 @@ public class FizzBuzz {
         String str;
         for(int i = 1; i <= 100; i++) {
             str = "";
-            if (i % 3 == 0) {
-                str = "Fizz";
-            }
-            if (i % 5 == 0) {
-                str += "Buzz";
-            }
-            if (str.isEmpty()) {
-                str += i;
-            }
+            if (i % 3 == 0) str = "Fizz";
+            if (i % 5 == 0) str += "Buzz";
+            if (str.isEmpty()) str += i;
             System.out.println(str);
         }
 
